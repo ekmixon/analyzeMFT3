631d68834b6d4bbc187ae60e6ff9eb89fb242f45
Brandon Helms <brandon.helms@renditioninfosec.com>
2019-10-10 13:52:58 -0400

    Updated to support python3.6+

---
+++ analyzemft/__init__.py
@@ -0,0 +1,6 @@
+__all__ = ["mftutils", "mft", "mftsession", "bitparse"]
+
+import analyzemft.bitparse
+import analyzemft.mft
+import analyzemft.mftsession
+import analyzemft.mftutils
---
+++ analyzemft/bitparse.py
@@ -0,0 +1,22 @@
+# This is all Willi Ballenthin's. Saved me a lot of headaches
+
+def parse_little_endian_signed_positive(buf):
+    ret = 0
+    for i, b in enumerate(buf):
+        ret += b * (1 << (i * 8))
+    return ret
+
+def parse_little_endian_signed_negative(buf):
+    ret = 0
+    for i, b in enumerate(buf):
+        ret += (b ^ 0xFF) * (1 << (i * 8))
+    ret += 1
+
+    ret *= -1
+    return ret
+
+def parse_little_endian_signed(buf):
+    if not buf[-1] & 0b10000000:
+        return parse_little_endian_signed_positive(buf)
+    else:
+        return parse_little_endian_signed_negative(buf)
