698285ab27697f897f0c80fd784ace84b25e897f
Sourcery AI <>
2022-08-02 18:49:21 +0000

    'Refactored by Sourcery'

--- analyzemft/bitparse.py
+++ analyzemft/bitparse.py
@@ -1,22 +1,18 @@
 # This is all Willi Ballenthin's. Saved me a lot of headaches
 
 def parse_little_endian_signed_positive(buf):
-    ret = 0
-    for i, b in enumerate(buf):
-        ret += b * (1 << (i * 8))
-    return ret
+    return sum(b * (1 << (i * 8)) for i, b in enumerate(buf))
 
 def parse_little_endian_signed_negative(buf):
-    ret = 0
-    for i, b in enumerate(buf):
-        ret += (b ^ 0xFF) * (1 << (i * 8))
+    ret = sum((b ^ 0xFF) * (1 << (i * 8)) for i, b in enumerate(buf))
     ret += 1
 
     ret *= -1
     return ret
 
 def parse_little_endian_signed(buf):
-    if not buf[-1] & 0b10000000:
-        return parse_little_endian_signed_positive(buf)
-    else:
-        return parse_little_endian_signed_negative(buf)
+    return (
+        parse_little_endian_signed_negative(buf)
+        if buf[-1] & 0b10000000
+        else parse_little_endian_signed_positive(buf)
+    )
